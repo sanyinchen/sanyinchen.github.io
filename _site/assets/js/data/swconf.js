e255b58e23f805dc8fca49882b80762a79e694a3
sanyinchen <sanyinchen@baidu.com>
2026-03-05 16:03:36 +0800

    [update] dp题单

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1772682493',resources: [
+    cacheName: 'chirpy-1772697544',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
